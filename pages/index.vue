6a84a06b2d626fd43bd8888d51a501363d7575f5
amanansari358 <168085099+amanansari358@users.noreply.github.com>
2024-06-06 16:20:43 +0545

    Nuxt Js demo page layout

---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <section class="bg-green-700">
+      <div class="container mx-auto px-20 py-10">
+        <!-- nav bar -->
+        <div class="flex text-white justify-between">
+          <a href>Realstore</a>
+          <div class="flex gap-8">
+            <a href>Home</a>
+            <a href>Product</a>
+          </div>
+          <a href>Login</a>
+          <!-- nav nbar end -->
+        </div>
+      </div>
+    </section>
+
+    <!-- Hero Section Start -->
+    <section>
+      <div class="container mx-auto px-20 py-10 text-white-400 flex justify-between items-center">
+        <div>
+          <img class="h-[500px]" src="../assets/img/download__3_-removebg-preview (1).png " alt />
+        </div>
+
+        <div class="display-flex justify-between">
+          <h1 class="text-black font-bold">NO.STORE IN THE WORLD !</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, aliquam.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+        </div>
+        <div class="bg-red-500  ">
+          <button>Explore</button>
+        </div>
+      </div>
+    </section>
+    <!-- Hero Section ENd  -->
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+</style>
